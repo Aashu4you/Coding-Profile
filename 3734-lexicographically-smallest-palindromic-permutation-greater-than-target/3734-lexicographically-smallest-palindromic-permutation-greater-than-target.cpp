555e38e71453911826af92e81c13c96bc47a2533
Aashutosh Sharma <ats4219511@gmail.com>
2026-08-28 08:47:06 +0530

    Time: 219 ms (6.15%), Space: 132.3 MB (6.15%) - LeetHub

---
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    string lexPalindromicPermutation(string s, string target) {
+
+        vector<int> cnt(26, 0);
+
+        for (char c : s)
+            cnt[c - 'a']++;
+
+        int odd = 0;
+        char mid = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] % 2 == 1) {
+                odd++;
+                mid = char('a' + i);
+            }
+        }
+
+        if (odd > 1)
+            return "";
+
+        vector<int> half(26);
+
+        for (int i = 0; i < 26; i++)
+            half[i] = cnt[i] / 2;
+
+        int n = s.size();
+        int len = n / 2;
+
+        string left = "";
+
+        for (int pos = 0; pos < len; pos++) {
+
+            bool found = false;
+
+            for (int c = 0; c < 26; c++) {
+
+                if (half[c] == 0)
+                    continue;
+
+                half[c]--;
+                left += char('a' + c);
+
+                string temp = left;
+
+                for (int x = 25; x >= 0; x--) {
+                    for (int k = 0; k < half[x]; k++)
+                        temp += char('a' + x);
+                }
+
+                string pal = temp;
+
+                if (n % 2 == 1)
+                    pal += mid;
+
+                for (int i = temp.size() - 1; i >= 0; i--)
+                    pal += temp[i];
+
+                if (pal > target) {
+                    found = true;
+                    break;
+                }
+
+                left.pop_back();
+                half[c]++;
+            }
+
+            if (!found)
+                return "";
+        }
+
+        string ans = left;
+
+        if (n % 2 == 1)
+            ans += mid;
+
+        for (int i = left.size() - 1; i >= 0; i--)
+            ans += left[i];
+
+        return ans > target ? ans : "";
+    }
+};
