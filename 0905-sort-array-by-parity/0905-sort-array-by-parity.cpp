5210bcf9e8e99a1aaa47ac8f03ad7df72b06641f
Aashutosh Sharma <ats4219511@gmail.com>
2026-09-11 08:19:48 +0530

    Time: 7 ms (3.88%), Space: 21.1 MB (5.92%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> odd;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2!=0){
+                odd.push_back(nums[i]);
+            }else{
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int x:odd){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
